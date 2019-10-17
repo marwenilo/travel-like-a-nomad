35bedbdec41a6f88103e0d51050aba28072d7ffb
amira04 <amira_soltani@yahoo.fr>
2019-10-17 22:06:50 +0100

    homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Route from "./route"
 import './App.css';
-
 function App() {
   return (
     <div className="App">
+      
      <Route />
     </div>
   );
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,31 @@
 import React from 'react';
 // import './Home.css';
-import NavBar from './NavBar'
+import NavBar from './components/NavBar'
+import ImageSlider from './components/ImageSlider'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faIgloo } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faIgloo)
 
 const Home = () => (
 
 <div>
      <NavBar /> 
+     <ImageSlider />
+      <p className="title2">What makes us different </p>
+
+      <div className="bodySlider"> 
+      <div>
+      <i class="fas fa-globe-asia"></i>
+      <p className="title3"> Discover the world </p>
+          </div> 
+      <div> <p className="title3"> Share a wealth of experience </p></div> 
+      <div><p className="title3"> Nothing is easier than travelling </p></div> 
+
+          </div> 
+
+
     </div>
   
 );
